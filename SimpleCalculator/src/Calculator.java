91d7427135c070db6ba7d160e276dadf618421d9
Tetiana Kizan <t.kizan@studenti.unina.it>
2020-04-21 00:27:36 +0200

    create project simple calculator

---
+++ SimpleCalculator/src/Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator{
+ 
+  public Calculator(){
+
+  }
+ 
+  public int subtract(int a, int b){
+    int result = a-b;
+    return result;
+  }
+  public int multiply(int a, int b){
+    int result = a*b;
+    return result;
+  }
+  public int divide(int a, int b){
+    int result = a/b;
+    return result;
+  }
+  public int modulo(int a, int b){
+    int result = a%b;
+    return result;
+  }
+public int add(int a, int b) {
+	  int result = a+b;
+	    return result;
+}
+  
+}
+
+ 
---
+++ SimpleCalculator/src/TestCalculator.java
@@ -0,0 +1,9 @@
+
+public class TestCalculator {
+	public static void main(String[] args){
+		   Calculator myCalculator = new Calculator();
+		   int addition = myCalculator.add(5, 7);
+		   System.out.println(addition);
+		// System.out.println(myCalculator.add(5, 7));
+		 }
+}
